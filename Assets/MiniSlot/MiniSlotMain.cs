3bdf817afd7aab97fafd68bec6055766a7976832
BorisKrylov <ibi692009@yandex.ru>
2026-02-17 13:52:27 +0300

    Add Task Scene and Entry point

---
+++ Assets/MiniSlot/MiniSlotMain.cs
@@ -0,0 +1,33 @@
+﻿using AxGrid;
+using AxGrid.Base;
+using AxGrid.FSM;
+using UnityEngine;
+
+namespace MiniSlot
+{
+    public class MiniSlotMain : MonoBehaviourExtBind
+    {
+        private MiniSlotView view;
+
+        [OnAwake]
+        private void AwakeThis()
+        {
+            Log.Debug("ExMain Awake");
+        }
+        
+        [OnStart]
+        private void StartThis()
+        {
+            Log.Debug("ExMain Start");
+
+            Settings.Fsm = new FSM();
+            MiniSlotFsm.Install(Settings.Fsm);
+        }
+
+        [OnUpdate]
+        private void UpdateThis()
+        {
+            Settings.Fsm.Update(Time.deltaTime);
+        }
+    }
+}
